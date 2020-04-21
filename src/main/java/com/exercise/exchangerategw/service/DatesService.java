5c453c4679aaa9b7e2e860566daf98d9897cb18b
Colin Greenwood <colin.greenwood@dwp.gsi.gov.uk>
2020-04-21 20:37:02 +0100

    Add DateService to calculate valid dates going back 1 month at a time.

---
+++ src/main/java/com/exercise/exchangerategw/service/DatesService.java
@@ -0,0 +1,18 @@
+package com.exercise.exchangerategw.service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class DatesService {
+
+  /*
+  We need to calculate valid dates going back 1 month at a time for 6 months
+  i.e. 31-05 goes back to 30-04
+   */
+  public List<LocalDate> calcTargetDates(LocalDate startDate) {
+    return IntStream.range(0, 6).boxed().map(n -> startDate.minusMonths(n)).collect(Collectors.toList());
+  }
+
+}
---
+++ src/test/java/com/exercise/exchangerategw/service/DatesServiceTest.java
@@ -0,0 +1,29 @@
+package com.exercise.exchangerategw.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DatesServiceTest {
+  DatesService datesService;
+
+  @Before
+  public void before(){
+    datesService =  new DatesService();
+  }
+
+  @Test
+  public void shouldCalculateLast6MonthsDates(){
+    List<LocalDate> dateList = datesService.calcTargetDates(LocalDate.of(2020, 7, 31));
+    assertEquals(6, dateList.size());
+    assertEquals("2020-07-31", dateList.get(0).toString());
+    assertEquals("2020-06-30", dateList.get(1).toString());
+    assertEquals("2020-05-31", dateList.get(2).toString());
+    assertEquals("2020-04-30", dateList.get(3).toString());
+    assertEquals("2020-03-31", dateList.get(4).toString());
+    assertEquals("2020-02-29", dateList.get(5).toString());
+  }
+}
