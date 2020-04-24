7336749f5fd5a51f8fc8e3e0fbd565eb074278d0
Colin Greenwood <colin.greenwood@dwp.gsi.gov.uk>
2020-04-24 07:31:44 +0100

    Move api into exchangerategw subdirectory.

--- src/main/java/com/exercise/exchangerategw/exceptions/MappingException.java
+++
@@ -1,9 +0,0 @@
-package com.exercise.exchangerategw.exceptions;
-
-public class MappingException extends RuntimeException {
-
-    public MappingException(String message) {
-        super(message);
-    }
-
-}
