b2a3f10fdebabb809a44659b07ac3a93c53f63b9
Colin Greenwood <colin.greenwood@dwp.gsi.gov.uk>
2020-04-25 09:21:15 +0100

    Rename public spec to just spec-api.yaml. Add basic auth to incoming requests. Allow spec unauth. Set to OFF.

--- exchangerateui/src/main/java/com/exercise/exchangerateui/client/ExternalApiClient.java
+++ exchangerateui/src/main/java/com/exercise/exchangerateui/client/ExternalApiClient.java
@@ -3,8 +3,10 @@ package com.exercise.exchangerateui.client;
 import client.exchangerate.DefaultApi;
 import client.exchangerate.ExchangeRates;
 import client.swagger.ApiException;
+import exceptions.AuthenticationException;
 import exceptions.ClientException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -16,13 +18,19 @@ public class ExternalApiClient {
     this.defaultApi = defaultApi;
   }
 
-  public ExchangeRates getExchangeRates(int months) {
+  public ExchangeRates getExchangeRates(int months) throws AuthenticationException {
     log.info("Requesting external exchange rates for {} months", months);
     try {
       return defaultApi.exchangeratesGet(months);
     } catch (ApiException e) {
-      log.error("Failed to parse client response to ClientExchangeRates.class", e);
-      throw new ClientException("Failed to parse client response");
+      if(e.getCode()== HttpStatus.UNAUTHORIZED.value()){
+        log.error("ApiException retrieving exchange rates:{}", e.getResponseBody(), e);
+        throw new AuthenticationException("Failed to parse client response");
+      }
+      else {
+        log.error("ApiException retrieving exchange rates:{}", e.getResponseBody(), e);
+        throw new ClientException("Failed to parse client response");
+      }
     }
   }
 
