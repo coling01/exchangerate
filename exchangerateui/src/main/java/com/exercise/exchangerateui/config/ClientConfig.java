e3c9cbe45a7a1cabb782c8e590b5531a8f21ea72
Colin Greenwood <colin.greenwood@dwp.gsi.gov.uk>
2020-04-27 08:23:48 +0100

    Enabled web and api security and api.

--- exchangerateui/src/main/java/com/exercise/exchangerateui/config/ClientConfig.java
+++ exchangerateui/src/main/java/com/exercise/exchangerateui/config/ClientConfig.java
@@ -2,6 +2,7 @@ package com.exercise.exchangerateui.config;
 
 import client.exchangerate.DefaultApi;
 import client.swagger.ApiClient;
+import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,18 +13,30 @@ public class ClientConfig {
   @Value("${application.apiclient.address}")
   private String apiClientUrl;
 
+  @Value("${application.apiclient.user}")
+  private String apiClientUser;
+
+  @Value("${application.apiclient.password}")
+  private String apiClientPassword;
+
   @Bean
-  public String apiClientUrl(){
+  public String apiClientUrl() {
     return apiClientUrl;
   }
 
   @Bean
-  public DefaultApi defaultApi(){
+  public DefaultApi defaultApi() {
     DefaultApi defaultApi = new DefaultApi();
-    ApiClient apiClient= new ApiClient();
+    ApiClient apiClient = new ApiClient();
     apiClient.setBasePath(apiClientUrl);
+    apiClient.addDefaultHeader("Authorization", "Basic " + encodedAuth());
     defaultApi.setApiClient(apiClient);
     return defaultApi;
+  }
+
+  private String encodedAuth(){
+    String auth = apiClientUser + ":" + apiClientPassword;
+    return Base64.getEncoder().encodeToString(auth.getBytes());
   }
 
 }
--- exchangerateui/src/main/java/com/exercise/exchangerateui/rest/ApiExceptionHandler.java
+++ exchangerateui/src/main/java/com/exercise/exchangerateui/rest/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.exercise.exchangerateui.rest;
 
+import exceptions.AuthenticationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,12 @@ import org.springframework.web.context.request.WebRequest;
 @Slf4j
 @ControllerAdvice
 public class ApiExceptionHandler {
+
+  @ExceptionHandler(value = AuthenticationException.class)
+  protected ResponseEntity<Object> handleAuthentException(Exception e, WebRequest req) {
+    log.error("Error caught in rest layer", e);
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+  }
 
   @ExceptionHandler(value = Exception.class)
   protected ResponseEntity<Object> handleException(Exception e, WebRequest req) {
